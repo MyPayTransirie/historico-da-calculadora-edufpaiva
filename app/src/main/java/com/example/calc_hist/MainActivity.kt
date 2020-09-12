c8e983eb23199abb12a01c37f885e47d264d7d0f
edufpaiva <edufpaiva33@gmail.com>
2020-09-12 13:38:59 -0400

    fun addSymbol

--- app/src/main/java/com/example/calc_hist/MainActivity.kt
+++ app/src/main/java/com/example/calc_hist/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.calc_hist
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 val multPat = Regex("[0-9.]+[x][0-9.]+")
 val divPat = Regex("[0-9.]+[/][0-9.]+")
@@ -14,60 +16,78 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
+        btn_div.setOnClickListener(){ addSymbol("/")}
+        btn_minus.setOnClickListener(){ addSymbol("-")}
+        btn_mult.setOnClickListener(){ addSymbol("x")}
+        btn_plus.setOnClickListener(){ addSymbol("+")}
 
     }
 
+    fun addSymbol(symbol: String) {
+        var txt = txt_prev.text.toString()
 
+        if (txt.isEmpty()) {
+            if (symbol == "-") txt += "-"
+        } else {
+            val lastChar = txt.drop(txt.lastIndex)
+            txt = txt.substring(0..(txt.lastIndex - 1))
+            if (simbPat.find(lastChar) == null) {
+                txt += lastChar
+            }
+            txt += symbol
+        }
+        if (txt.length == 1)
+            if ((txt != "-" && simbPat.find(txt) != null))
+                txt = ""
 
-
-}
-
-fun eval( op_txt: String): Double {
-
-    var txt = op_txt
-
-    while (multPat.find(txt) != null){
-        val res = multPat.find(txt)?.value.toString()
-        val result = getResult(res)
-        txt = txt.replace(res, result.toString())
+        txt_prev.text = txt
     }
 
-    while (divPat.find(txt) != null){
-        val res = divPat.find(txt)?.value.toString()
-        val result = getResult(res)
-        txt = txt.replace(res, result.toString())
+    fun eval(op_txt: String): Double {
+
+        var txt = op_txt
+
+        while (multPat.find(txt) != null) {
+            val res = multPat.find(txt)?.value.toString()
+            val result = getResult(res)
+            txt = txt.replace(res, result.toString())
+        }
+
+        while (divPat.find(txt) != null) {
+            val res = divPat.find(txt)?.value.toString()
+            val result = getResult(res)
+            txt = txt.replace(res, result.toString())
+        }
+
+        while (plusPat.find(txt) != null) {
+            val res = plusPat.find(txt)?.value.toString()
+            val result = getResult(res)
+            txt = txt.replace(res, result.toString())
+        }
+
+        while (minPat.find(txt) != null) {
+            val res = minPat.find(txt)?.value.toString()
+            val result = getResult(res)
+            txt = txt.replace(res, result.toString())
+        }
+
+        return txt.toDouble()
+
     }
 
-    while (plusPat.find(txt) != null){
-        val res = plusPat.find(txt)?.value.toString()
-        val result = getResult(res)
-        txt = txt.replace(res, result.toString())
+    fun getResult(txt: String): Double {
+
+        val numbers = txt.split(simbPat).map { x -> x.toDouble() }
+        val symbol = simbPat.find(txt)?.value
+
+        when (symbol) {
+            "+" -> return numbers[0] + numbers[1]
+            "-" -> return numbers[0] - numbers[1]
+            "x" -> return numbers[0] * numbers[1]
+            "/" -> return numbers[0] / numbers[1]
+        }
+        return 0.0
     }
 
-    while (minPat.find(txt) != null){
-        val res = minPat.find(txt)?.value.toString()
-        val result = getResult(res)
-        txt = txt.replace(res, result.toString())
-    }
-
-    return txt.toDouble()
-
-}
-
-
-fun getResult(txt:String):Double{
-
-    val numbers = txt.split(simbPat).map { x -> x.toDouble() }
-    val symbol = simbPat.find(txt)?.value
-
-    when(symbol){
-        "+" -> return numbers[0] + numbers[1]
-        "-" -> return numbers[0] - numbers[1]
-        "x" -> return numbers[0] * numbers[1]
-        "/" -> return numbers[0] / numbers[1]
-    }
-    return 0.0
 }
 
